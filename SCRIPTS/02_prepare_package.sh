ce172803197ba6951e21dfc4588aaa8ec6456c98
dayuchun42 <110729991+dayuchun42@users.noreply.github.com>
2024-08-15 17:57:46 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -299,6 +299,26 @@ rm -rf ./feeds/packages/net/sing-box
 cp -rf ../immortalwrt_pkg/net/sing-box ./feeds/packages/net/sing-box
 # OpenClash
 git clone --single-branch --depth 1 -b master https://github.com/vernesong/OpenClash.git package/new/luci-app-openclash
+
+# 预置Clash内核
+echo -e "预置Clash内核"
+mkdir -p package/luci-app-openclash/root/etc/openclash/core
+core_path="package/luci-app-openclash/root/etc/openclash/core"
+goe_path="package/luci-app-openclash/root/etc/openclash"
+
+CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
+CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep "amd64" | awk -F '"' '{print $4}' | grep "v3" )
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+
+wget -qO- $CLASH_DEV_URL | tar xOvz > $core_path/clash
+wget -qO- $CLASH_TUN_URL | gunzip -c > $core_path/clash_tun
+wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
+wget -qO- $GEOIP_URL > $goe_path/GeoIP.dat
+wget -qO- $GEOSITE_URL > $goe_path/GeoSite.dat
+
+chmod +x $core_path/clash*
 # Passwall
 cp -rf ../passwall_luci/luci-app-passwall ./package/new/luci-app-passwall
 wget -P package/new/luci-app-passwall/ https://github.com/QiuSimons/OpenWrt-Add/raw/master/move_2_services.sh
